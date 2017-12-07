2ed0b60a508f3b3d59eaa92432d3f62ad0631677
scumworth <danmcninch@gmail.com>
2017-12-07 12:18:41 -0500

    changes site url

--- client/src/index.js
+++ client/src/index.js
@@ -4,22 +4,21 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import './index.css';
 import App from './containers/App';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import './index.css';
 import store, { history } from './store';
 console.log('test');
 
 //import registerServiceWorker from './registerServiceWorker';
 
-console.log(process.env.TEST);
 ReactDOM.render(
     <Provider store = {store}>
         <ConnectedRouter history = {history}>
             <div>
             <App
-                url = 'https://agile-hollows-85089.herokuapp.com/api'
+                url = 'https://yelpnightlifeapp.herokuapp.com//api'
             />
             </div>
         </ConnectedRouter>
