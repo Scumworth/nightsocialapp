c7f65e19cd0a7e740927ed580f4c8b8e85ba1f93
Scumworth <danmcninch@gmail.com>
2018-05-03 18:03:33 -0400

    edits bootstrap theme import

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 // index.js
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
